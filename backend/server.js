8c6ab5e8471194c940fba9222fe975e3f3d97390
vc743 <victorcontrerasvargas55@gmail.com>
2025-05-15 17:27:00 -0400

    add routes

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const workoutRoutes = require("./routes/workouts");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+app.use("/api/workouts", workoutRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Listening on port ${process.env.PORT}`);
+});
