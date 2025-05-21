61166d2970ae8eaa10543f36e6f98c5e5e8e7d4f
vc743 <victorcontrerasvargas55@gmail.com>
2025-05-21 13:08:03 -0400

    feat: add Workout Context to keep the database and local state synchronized

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 import WorkoutDetails from "../components/WorkoutDetails";
 import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
-  const [workouts, setWorkouts] = useState(null);
+  const { workouts, dispatch } = useWorkoutsContext();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
@@ -12,7 +13,7 @@ const Home = () => {
       const json = await response.json();
 
       if (response.ok) {
-        setWorkouts(json);
+        dispatch({ type: "SET_WORKOUTS", payload: json })
       }
     }
 
@@ -23,7 +24,7 @@ const Home = () => {
     <div className="home">
       <div className="workouts">
         {workouts && workouts.map(workout => (
-          <WorkoutDetails key={workout._id} workout={workout}/>
+          <WorkoutDetails key={workout._id} workout={workout} />
         ))}
       </div>
       <WorkoutForm />
