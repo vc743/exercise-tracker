00415704f6c1346c35ebf688136ed0aa06029187
vc743 <victorcontrerasvargas55@gmail.com>
2025-05-20 22:12:46 -0400

    feat: add WorkoutForm component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
+
 import WorkoutDetails from "../components/WorkoutDetails";
+import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const [workouts, setWorkouts] = useState(null);
@@ -24,6 +26,7 @@ const Home = () => {
           <WorkoutDetails key={workout._id} workout={workout}/>
         ))}
       </div>
+      <WorkoutForm />
     </div>
   )
 }
