d66b26563c9cd086a9d9b724196adb310ff51bd9
vc743 <victorcontrerasvargas55@gmail.com>
2025-05-21 15:31:37 -0400

    feat: add better error message when fields are empty and better date format

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -7,6 +7,7 @@ const WorkoutForm = () => {
     const [load, setLoad] = useState("");
     const [reps, setReps] = useState("");
     const [error, setError] = useState(null);
+    const [emptyFields, setEmptyFields] = useState([]);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -25,6 +26,7 @@ const WorkoutForm = () => {
 
         if (!response.ok) {
             setError(json.error);
+            setEmptyFields(json.emptyFields);
         }
 
         if (response.ok) {
@@ -32,6 +34,7 @@ const WorkoutForm = () => {
             setLoad("");
             setReps("");
             setError(null);
+            setEmptyFields([]);
             console.log("new workout added", json);
             dispatch({ type: "CREATE_WORKOUT", payload: json });
         }
@@ -46,18 +49,21 @@ const WorkoutForm = () => {
                 type="text"
                 onChange={(e) => setTitle(e.target.value)}
                 value={title}
+                className={emptyFields.includes("title") ? "error" : ""}
             />
             <label>Load (in Kg):</label>
             <input
                 type="number"
                 onChange={(e) => setLoad(e.target.value)}
                 value={load}
+                className={emptyFields.includes("load") ? "error" : ""}
             />
             <label>Reps:</label>
             <input
                 type="number"
                 onChange={(e) => setReps(e.target.value)}
                 value={reps}
+                className={emptyFields.includes("reps") ? "error" : ""}
             />
             <button>Add Workout</button>
             {error && <div className="error">{error}</div>}
