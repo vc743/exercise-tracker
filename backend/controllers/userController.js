752bed215741104b2803bbd414a654652af5d5c5
vc743 <victorcontrerasvargas55@gmail.com>
2025-06-14 16:22:26 -0400

    feat: add login static method on user model and update the loginUser controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,21 @@ const jwt = require("jsonwebtoken");
 
 const createToken = (_id) => {
   return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
-}
+};
 
 // login user
 const loginUser = async (req, res) => {
-  res.json({ mssg: "login user" });
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 };
 
 // signup user
