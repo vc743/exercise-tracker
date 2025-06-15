337f6de9bc053e5d0d99c594c2625f7f73676ce3
vc743 <victorcontrerasvargas55@gmail.com>
2025-06-15 16:36:43 -0400

    feat: add AuthContext and AuthContext hook

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { WorkoutsContextProvider } from './context/WorkoutContext.jsx'
+import { AuthContextProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WorkoutsContextProvider>
-      <App />
-    </WorkoutsContextProvider>
+    <AuthContextProvider>
+      <WorkoutsContextProvider>
+        <App />
+      </WorkoutsContextProvider>
+    </AuthContextProvider>
   </StrictMode>,
 )
