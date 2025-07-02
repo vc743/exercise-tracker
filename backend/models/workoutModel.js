c0db9e2946cc83525d2230c84d0f1b679eceadf4
vc743 <victorcontrerasvargas55@gmail.com>
2025-07-01 22:39:45 -0400

    feat: filter workouts by user ID to show only user's own entries

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -2,19 +2,26 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+    user_id: {
+      type: String,
+      require: true,
+    },
   },
-  reps: {
-    type: Number,
-    required: true,
-  },
-  load: {
-    type: Number,
-    required: true,
-  },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Workout", workoutSchema);
